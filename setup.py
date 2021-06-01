e2626dbcc0cf06d8b6445658eabf894321eecb8d
Anton Anisimov <raprek1@gmail.com>
2021-06-01 23:34:20 +0300

    setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='dictToJSONWidget',
+    author='Anton Anisimov',
+    author_email='raprek1@gmail.com',
+    packages=['dictToJSONWidget'],
+    version='0.1',
+    license='MIT',
+    description='The util helping to avoid annoying handwork of data transformation into json scheme for JSONEditorWidget',
+)
